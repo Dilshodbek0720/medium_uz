05e40e94554596c5b9bfd8380ea160216b6ca280
Dilshodbek0720 <sayitqulovdilshodbek@gmail.com>
2023-08-22 08:15:12 +0500

    Initial commit

---
+++ lib/presentation/app_routes.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RouteNames {
+  static const String splashScreen = "/";
+  static const String authScreen = "/auth_screen";
+  static const String tabBox = "/tab_box";
+  static const String confirmGmail = "/confirm_gmail";
+}
+
+class AppRoutes {
+  static Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      // case RouteNames.splashScreen:
+      //   return MaterialPageRoute(builder: (context) => SplashScreen());
+      // case RouteNames.authScreen:
+      //   return MaterialPageRoute(builder: (context) {
+      //     return AuthScreen();
+      //   });
+      //
+      // case RouteNames.tabBox:
+      //   return MaterialPageRoute(builder: (context) => TabBox());
+      //
+      // case RouteNames.confirmGmail:
+      //   return MaterialPageRoute(builder: (context) => GmailConfirmScreen());
+
+      default:
+        return MaterialPageRoute(
+          builder: (context) => const Scaffold(
+            body: Center(
+              child: Text("Route mavjud emas"),
+            ),
+          ),
+        );
+    }
+  }
+}
